0f64f5f876faf49aae5580cee0dbc979318a4991
Amin Adibi <adibi@alumni.ubc.ca>
2020-11-25 15:30:12 -0800

    random generation updated

--- R/bridge_prism.R
+++ R/bridge_prism.R
@@ -2,41 +2,38 @@ model_run <- function() {
 
   patient <- list()
 
-  coin <- runif(1, min = 0, max = 1)
+  if (runif(1, min = 0, max = 1) >= 0.5) {
+    patient$male <- "Female"
+  } else {patient$male <- "Male"}
 
-  if (coin >= 0.5) {
-    patient$male <- 0
-  } else {patient$male <- 1}
+  if (runif(1, min = 0, max = 1) >= 0.258) {
+    patient$smoker <- "Non-Smoker"
+  } else {patient$smoker <- "Smoker"}
 
-  if (coin >= 0.258) {
-    patient$smoker <- 0
-  } else {patient$smoker <- 1}
+  if (runif(1, min = 0, max = 1) >= 0.4685) {
+    patient$oxygen <- "No"
+  } else {patient$oxygen <- "Yes"}
 
-  if (coin >= 0.4685) {
-    patient$oxygen <- 0
-  } else {patient$oxygen <- 1}
+  if (runif(1, min = 0, max = 1) >= 0.2265) {
+    patient$statin <- "No"
+  } else {patient$statin <- "Yes"}
 
-  if (coin >= 0.2265) {
-    patient$statin <- 0
-  } else {patient$statin <- 1}
+  if (runif(1, min = 0, max = 1) >= 0.6504) {
+    patient$LAMA <- "No"
+  } else {patient$LAMA <- "Yes"}
 
-  if (coin >= 0.6504) {
-    patient$LAMA <- 0
-  } else {patient$LAMA <- 1}
+  if (runif(1, min = 0, max = 1) >= 0.5206) {
+    patient$LABA = "No"
+  } else {patient$LABA <- "Yes"}
 
-  if (coin >= 0.5206) {
-    patient$LABA = 0
-  } else {patient$LABA <- 1}
-
-  if (coin >= 0.5723) {
-    patient$ICS = 0
-  } else {patient$ICS <- 1}
+  if (runif(1, min = 0, max = 1) >= 0.5723) {
+    patient$ICS = "No"
+  } else {patient$ICS <- "Yes"}
 
   patient$age  <- round(rnorm(1, mean = 64.68, sd = 8.75))
   patient$FEV1 <- round(rnorm(1, mean = 40.60, sd = 15.93))
   patient$BMI  <- round(rnorm(1, mean = 27.53, sd = 6.43))
   patient$SGRQ <- round(rnorm(1, mean = 49.95, sd = 16.72))
-
 
   #patient$LastYrExacCount <- rpois (n = 1, lambda = 1.42)
   #patient$LastYrSevExacCount <- rpois (n = 1, lambda = 0.29)
